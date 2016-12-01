04489fee9352623b31bcbe65caaeafad902e1afc
richardjoin <2547900199@qq.com>
2016-12-01 11:54:13 +0800

    12-1one

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ class App extends React.Component {
     })
   }
   render () {
-    console.log(this.props);
     return(
       <div className='my-wrap'>
         <NavHeader title={this.state.title} />
