fcab77b938fd9dd9039030d0728db9e1287e5c86
richardjoin <2547900199@qq.com>
2016-12-03 13:38:30 +0800

    120313

--- src/Blog/index.js
+++ src/Blog/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 
+import axios from 'axios';
+
+import Loading from '../component/Loading';
 import BlogCard from '../component/BlogCard';
-let data = [
-  {index:1,title:'hello1',desc:'word1'},
-  {index:2,title:'hello2',desc:'word2'},
-  {index:3,title:'hello3',desc:'word3'},
-  {index:4,title:'hello4',desc:'word4'},
-  {index:5,title:'hello5',desc:'word5'},
-  {index:6,title:'hello6',desc:'word6'},
-  {index:7,title:'hello7',desc:'word7'},
-  {index:8,title:'hello8',desc:'word8'}
-]
 // {...item}自动把data展开,等同index=data.index;
 class Blog extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      data:[],
+      wait:true
+    }
+  }
+  componentDidMount(){
+    axios.get('https://raw.githubusercontent.com/richardjoin/08-demo/master/data/blogcard.json?a='+Math.random())
+    .then( res => this.setState({data:res.data,wait:false}) )
+  }
   render () {
-    let cards = data.map( (item,i) => <BlogCard {...item} key={i} />)
     return(
       <div className='blog-warp'>
-      {cards}
+        {this.state.wait ? <Loading /> :
+          this.state.data.map( (item,i) => <BlogCard {...item} key={i} /> )
+        }
       </div>
     )
   }
