f0512fcd691096f35f34d54320e83fa17cb72ee8
richardjoin <2547900199@qq.com>
2016-12-04 14:32:13 +0800

    9

--- src/component/LeftNav.js
+++ src/component/LeftNav.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import {Link} from 'react-router';
 
@@ -6,10 +7,10 @@ class LeftNav extends React.Component {
     return(
       <div className='leftnav-wrap'>
         <h1>richardjoin@{this.props.title}</h1>
-        <Link to='/' activeStyle={{backgroundColor:'rgba(0.0.0.0.5)'}} onlyActiveOnIndex={true}>Nome</Link>
-        <Link to='blog' activeStyle={{backgroundColor:'rgba(0.0.0.0.5)'}}>Blog</Link>
-        <Link to='work' activeStyle={{backgroundColor:'rgba(0.0.0.0.5)'}}>Work</Link>
-        <Link to='about' activeStyle={{backgroundColor:'rgba(0.0.0.0.5)'}}>About</Link>
+        <Link to='/' activeStyle={{backgroundColor:'rgba(0,0,0,0.5)'}} onlyActiveOnIndex={true}>Home</Link>
+        <Link to='blog' activeStyle={{backgroundColor:'rgba(0,0,0,0.5)'}}>Blog</Link>
+        <Link to='work' activeStyle={{backgroundColor:'rgba(0,0,0,0.5)'}}>Work</Link>
+        <Link to='about' activeStyle={{backgroundColor:'rgba(0,0,0,0.5)'}}>About</Link>
       </div>
     )
   }
